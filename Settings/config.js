f83b9052d3e342200120c9f140a2d4721b45e536
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-03 01:11:37 +0100

    Added note to config

--- Settings/config.js
+++ Settings/config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    
+    
+/*
+
+Want active support and new updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Discord-Channel-Moderator 
+
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+
+*/
+
+
 
     // ---------DISCORD ACCOUNT SETUP-----------//
 
