1c4cf525c76a3c604c18c89ecf69257bb7b739fc
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-09 23:41:04 +0100

    Added notes

--- app/Colors - Console/logcol.js
+++ app/Colors - Console/logcol.js
@@ -1,4 +1,44 @@
-// Colors
+
+// Copyright notice:
+
+/*--------------------------------------------------------------------------------------------- 
+* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+
+* Code origin: https://github.com/OSL-Works/Discord-Channel-Moderator
+* Developer name: Veljko Vuckovic
+* Licensed under the MIT License. See LICENSE in the project root for license information.
+* Published License: https://github.com/OSL-Works/Discord-Channel-Moderator/blob/master/LICENSE
+
+* Contact information:
+  Discord Support Server: https://discord.gg/D8WCtDD
+  Main developer steam: https://steamcommunity.com/id/MajokingGames/ 
+  Mail: refloowlibrarycontact@gmail.com
+
+* Donations:
+  Donate: https://ko-fi.com/refloow
+ --------------------------------------------------------------------------------------------*/
+
+ /* 
+
+// legal advice: PERMISSIONS AND RIGHTS
+
+* License does not prohibit modification, distribution, private/commercial use or sale of copies as long as the original LICENSE file
+ and authors copyright notice are left ass they are in the project files.
+* Copyright notice could be included ones or multiple times within the file.
+* Copyright notice should not be removed even within the larger works (Larger modifications applied).
+* Original file tags cannot be removed without creators exclusive permission.
+* Adding own tags in files is possible in case of modification - even in that case original tags must be kept.
+* Year on the copyright notice breakdown:
+* Generally, the “year of first publication of the work” refers to the year in which the work was first distributed to the public (first year mentioned)
+* Any year after represents the year of added modifications.
+* Copyright cannot expire so therefore you cannot remove copyright notice if its not updated to the latest year.
+
+===================================================================================
+Removing copyright notice & distributing, using or selling the software without
+the original license and copyright notice is licence agreement breach and its considered criminal offense and piracy.
+===================================================================================
+
+*/
 
 const winston = require('winston');
 const level ={
