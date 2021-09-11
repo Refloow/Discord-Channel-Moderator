9bb00701b42eeb2d9cd7aee4cb754dfeb9e63a8f
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-09-11 11:10:02 +0200

    Changed log message on error catch

--- app/app.js
+++ app/app.js
@@ -303,7 +303,7 @@ if(method.ChatFilterEnabled()) {
 //------------------------------------------------//
 ////////////////////////////////////////////////////
 
-refloow.login(config.Discord_Login).catch(() => Console.error("Invalid token."))
+refloow.login(config.Discord_Login).catch(() => Console.error("| [Discord] |: Invalid Bot Login Token."))
 
 
 // Copyright notice:
