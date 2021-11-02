7ae19b35ce008f7da0f1113ad9545203560f88ad
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-11-02 16:39:58 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -13,7 +13,7 @@ echo .
 echo // Copyright notice:
 echo .
 echo /* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
-echo  Code origin (Free GitHub publish): https://github.com/OSL-Works/Discord-Channel-Moderator */
+echo  Code origin (Free GitHub publish): https://github.com/Refloow/Discord-Channel-Moderator */
 echo .
 echo .
 explorer "https://discord.gg/D8WCtDD"
