ee47b6d0a5c4aa6c7dd4d87b2315f5f49d5eb607
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-11-06 11:02:03 +0100

    Fixing push error

--- index.js
+++ index.js
@@ -14,7 +14,11 @@
   Main developer steam: https://steamcommunity.com/id/MajokingGames/
   Mail: refloowlibrarycontact@gmail.com
 
+* Donations:
+  Crypto: https://refloow.com/cdonate
+  Steam: https://steamcommunity.com/tradeoffer/new/?partner=994828078&token=XEUdbqp6
  --------------------------------------------------------------------------------------------*/
+
  /*
 
 // legal advice: PERMISSIONS AND RIGHTS
@@ -35,304 +39,47 @@
 Removing copyright notice & distributing, using or selling the software without
 the original license and copyright notice is licence agreement breach and its considered criminal offense and piracy.
 ===================================================================================
-
 */
-
-
-
-
-
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//             Script checks, imports             //
-//------------------------------------------------//
-////////////////////////////////////////////////////
-
 
 // Checking if all modules are installed correctly
 
 try {
-  // Checking if discord module is installed correctly
-  Discord = require ('discord.js');
-  // Checking if console-master module is installed correctly
-  Console = require ('console-master');
+  colors = require('colors');
 } catch (ex) {
   console.log('\n\n\n | [Modules] |: Missing dependecies Run install.bat file or use npm install. \n\n\n');
   console.log(ex);
   process.exit(1);
 }
 
-// Setting other things for the project
+// Importing required files
+const method = require('./app/methods.js');
+const package = require('./package.json');
+const v = package.version;
 
-const config = require('../Settings/config.js');
-const method = require('./methods.js');
-const refloow = new Discord.Client();
-var servers = {};
+// Basic infomations display on startup
 
+console.log('8888888b.          .d888888                              '.cyan);
+console.log('888   Y88b        d88P" 888                              '.cyan);
+console.log('888    888        888   888                              '.cyan);
+console.log('888   d88P .d88b. 888888888 .d88b.  .d88b. 888  888  888'.cyan);
+console.log('8888888P" d8P  Y8b888   888d88""88bd88""88b888  888  888'.cyan);
+console.log('888 T88b  88888888888   888888  888888  888888  888  888'.cyan);
+console.log('888  T88b Y8b.    888   888Y88..88PY88..88PY88b 888 d88P '.cyan);
+console.log('888   T88b "Y8888 888   888 "Y88P"  "Y88P"  "Y8888888P" \n\n\n'.cyan);
+console.log('/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved. \n Code origin (Free GitHub publish): https://github.com/Refloow/Discord-Channel-Moderator*/\n'.cyan);
+console.log(`Verision v${v}`.cyan);
+console.log('Preparing code for launch'.cyan);
+console.log(''.red);
+console.log('Loading config file...\n\n'.green );
 
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//              Starting the client               //
-//------------------------------------------------//
-////////////////////////////////////////////////////
+// Starting main app
 
-// Client on 'ready'
-
-refloow.on('ready', () => {
-    Console.true("| [Discord] | Succesfully connected as " + refloow.user.tag)
-
-    refloow.user.setActivity(config.DisplayMessage, {type: config.Type})
-
-    refloow.guilds.forEach((guild) =>{
-        Console.true("| [Discord] | Connected to server: "+guild.name)
-        guild.channels.forEach((channel) => {
-        })
-    })
-})
+require('./app/app.js');
 
 
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//              Moderating links                  //
-//------------------------------------------------//
-////////////////////////////////////////////////////
+// Checking for correct version (updates) for bot on github
 
-
-// If moderating links is enabled proceed
-if(method.ModeratingLinks()) {
-  // on message
-  refloow.on("message", (message) => {
-    // check if message is sent in allowd channel
-   if (message.channel.id === config.Allowed_Channel_1) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_1)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-          } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-     // Check if channel is allowed channel
-    } else if (message.channel.id === config.Allowed_Channel_2) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl2(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_2)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-      // Check if channel is allowed channel
-    } else if (message.channel.id === config.Allowed_Channel_3) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl3(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_3)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-      // Check if channel is allowed channel
-    } else if (message.channel.id === config.Allowed_Channel_4) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl4(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_4)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-      // Check if channel is allowed channel
-    } else if (message.channel.id === config.Allowed_Channel_5) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl5(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_5)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-      // Check if channel is allowed channel
-    } else if (message.channel.id === config.Allowed_Channel_6) {
-     // check if the message in allowed channel has url
-    if (method.containsUrl(message.content)) {
-      // check if url is allowed
-      if (method.isAllowedUrl6(message.content)) {
-          } else {
-            // if it isnt allowed url
-            // tell user url is not allowed
-            if(method.MessageAfterDeleteLink()) {
-            message.author.send(config.MessageOnDelete_6)
-            }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-      } else {
-        // it's not an url
-        // do nothing
-      }
-    }
-
-     else {
-    }
-// Check if message is not in allowed channels
-if (!((message.channel.id == config.Allowed_Channel_1) || (message.channel.id == config.Allowed_Channel_2) || (message.channel.id == config.Allowed_Channel_3) || (message.channel.id == config.Allowed_Channel_4) || (message.channel.id == config.Allowed_Channel_5) || (message.channel.id == config.Allowed_Channel_6))) {
-  // Check if message contains url
-  if (method.containsUrl(message.content)) {
-    // Check configuration
-    if(method.MessageAfterDeleteLink()) {
-          message.author.send(config.GlobalDeleteURL)
-          }
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-            // then delete it
-            message.delete()
-            } else {}
-          }
-        }
-});
-};
-
-
-
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//     Cleaning regular chat from bot commands    //
-//------------------------------------------------//
-////////////////////////////////////////////////////
-
-// If this feature is enabled (checks config)
-if(method.DeletingCommands()) {
-  // On message event
-  refloow.on("message", (message) => {
-
-     // Importing Blacklisted prefixes from config
-     let blacklisted = config.blacklisted;
-
-      for (var i in blacklisted) {
-      // If message is command (contains prefix)
-      if (message.content.startsWith(blacklisted[i])) {
-          // Delete it
-          // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-          // Delete the message
-          message.delete()
-        // Check configuration
-        if(method.MessageAfterDelete()) {
-            // Message user if enabled
-            message.author.send(config.MessageAfterDelete);
-          }
-        } else {}
-  }
-      }
-  });
-}
-
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//         Blacklisted words filter               //
-//------------------------------------------------//
-////////////////////////////////////////////////////
-
-// Is this feature enabled (checks config)
-if(method.ChatFilterEnabled()) {
-  // On message event
-  refloow.on("message", (message) => {
-
-   // Importing words from config
-   let bannedwords = config.bannedwords;
-
-      for (var i in bannedwords) {
-      // If message contains bad words
-      if (message.content.toLowerCase().includes(bannedwords[i])) {
-          // Delete it
-                    // If user has required permissions in channel
-          if(message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES")) {
-          message.delete()
-        // Checks config
-        if(method.MessageAfterDeleteFilter()) {
-            // Messageu user if enabled
-            message.author.send(config.MessageAfterDelete_filter);
-          }
-        }  else {}
-  }
-      }
-  });
-}
-
-
-
-////////////////////////////////////////////////////
-//------------------------------------------------//
-//                  Logging in                    //
-//------------------------------------------------//
-////////////////////////////////////////////////////
-
-refloow.login(config.Discord_Login).catch(() => Console.error("| [Discord] |: Invalid Bot Login Token."))
-
+	method.check()
 
 // Copyright notice:
 
